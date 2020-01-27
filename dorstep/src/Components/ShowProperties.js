5aeace539a7249fb782d995942ac7ea95c9ad4f7
jadekang <jkang.se@gmail.com>
2020-01-26 23:50:04 -0800

    successfully added a modal screen function using useState to my propertycard component

--- dorstep/src/Components/ShowProperties.js
+++ dorstep/src/Components/ShowProperties.js
@@ -6,47 +6,6 @@ import { properties } from '../properties';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-import Modal from 'react-bootstrap/Modal';
-import Button from 'react-bootstrap/Button';
-
-function MydModalWithGrid(props) {
-    return (
-        <Modal {...props} aria-labelledby="contained-modal-title-vcenter">
-            <Modal.Header closeButton>
-                <Modal.Title id="contained-modal-title-vcenter">
-                    Using Grid in Modal
-        </Modal.Title>
-            </Modal.Header>
-            <Modal.Body>
-                <Container>
-                    <Row className="show-grid">
-                        <Col xs={12} md={8}>
-                            <code>.col-xs-12 .col-md-8</code>
-                        </Col>
-                        <Col xs={6} md={4}>
-                            <code>.col-xs-6 .col-md-4</code>
-                        </Col>
-                    </Row>
-
-                    <Row className="show-grid">
-                        <Col xs={6} md={4}>
-                            <code>.col-xs-6 .col-md-4</code>
-                        </Col>
-                        <Col xs={6} md={4}>
-                            <code>.col-xs-6 .col-md-4</code>
-                        </Col>
-                        <Col xs={6} md={4}>
-                            <code>.col-xs-6 .col-md-4</code>
-                        </Col>
-                    </Row>
-                </Container>
-            </Modal.Body>
-            <Modal.Footer>
-                <Button onClick={props.onHide}>Close</Button>
-            </Modal.Footer>
-        </Modal>
-    );
-}
 
 function ShowProperties() {
   console.log(properties);
@@ -61,13 +20,11 @@ function ShowProperties() {
           {properties.map(property => {
             return (
               <Col xs={6} md={4} lg={3}>
-                <Link to="/questions/payment">
-                  <PropertyCard
-                    image={property.images[0]}
-                    title={property.title}
-                    description={property.city}
-                  />
-                </Link>
+                <PropertyCard
+                  image={property.images[0]}
+                  title={property.title}
+                  description={property.city}
+                />
               </Col>
             );
           })}
--- dorstep/src/Widgets/PropertyCard.js
+++ dorstep/src/Widgets/PropertyCard.js
@@ -1,11 +1,66 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Card from 'react-bootstrap/Card';
+import Modal from 'react-bootstrap/Modal';
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
+function MydModalWithGrid(props) {
+  return (
+    <Modal
+      {...props}
+      size="lg"
+      aria-labelledby="contained-modal-title-vcenter"
+      centered
+    >
+      <Modal.Header closeButton>
+        <Modal.Title id="contained-modal-title-vcenter">
+          Using Grid in Modal
+        </Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <Container>
+          <Row className="show-grid">
+            <Col xs={12} md={8}>
+              <code>.col-xs-12 .col-md-8</code>
+            </Col>
+            <Col xs={6} md={4}>
+              <code>.col-xs-6 .col-md-4</code>
+            </Col>
+          </Row>
+
+          <Row className="show-grid">
+            <Col xs={6} md={4}>
+              <code>.col-xs-6 .col-md-4</code>
+            </Col>
+            <Col xs={6} md={4}>
+              <code>.col-xs-6 .col-md-4</code>
+            </Col>
+            <Col xs={6} md={4}>
+              <code>.col-xs-6 .col-md-4</code>
+            </Col>
+          </Row>
+        </Container>
+      </Modal.Body>
+      <Modal.Footer>
+        <Button onClick={props.onHide}>Close</Button>
+      </Modal.Footer>
+    </Modal>
+  );
+}
 
 function PropertyCard(props) {
+  const [modalShow, setModalShow] = useState(false);
   return (
     <>
+      <MydModalWithGrid show={modalShow} onHide={() => setModalShow(false)} />
       <Card>
-        <Card.Img variant="top" src={props.image} />
+        <Card.Img
+          variant="top"
+          src={props.image}
+          onClick={() => setModalShow(true)}
+        />
         <Card.Body>
           <Card.Title>{props.title}</Card.Title>
           <Card.Text>{props.details}</Card.Text>
